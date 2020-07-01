cd70e9e227b8821270a8464add6dade059638bd5
jessica880512 <55180625+jessica880512@users.noreply.github.com>
2020-07-01 11:45:00 +0800

    有傳了apk不知道行不行

---
+++ Food/Assets/Scripts/ChipBag.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChipBag : MonoBehaviour
+{
+    public static Transform target;
+    [Header("旋轉")]
+    public Transform ChipBagRotation;
+    [Header("閃爍")]
+    public ParticleSystem psShine;
+    [Header("血量"), Range(100, 500)]
+    public float hp;
+    [Header("血條")]
+    public Image hpBar;
+    [Header("結束畫面")]
+    public GameObject final;
+    [Header("數量")]
+    public Text textCount;
+    [Header("殭屍死亡音效")]
+    public AudioClip soundZombie;
+    [Header("洋芋片受傷音效")]
+    public AudioClip soundHit;
+
+    private int count;
+    private float hpMax;
+    private AudioSource aud;
+    private void Start()
+    {
+        aud = GetComponent<AudioSource>();
+        hpMax = hp;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Attack();
+    }
+    private void Attack()
+    { if (target && hp > 0)
+        {
+            Vector3 pos = target.position;
+            pos.y = ChipBagRotation.position.y;
+            psShine.Play();
+            ChipBagRotation.LookAt(pos);
+
+            count++;
+            aud.PlayOneShot(soundZombie);
+        }
+            }
+
+    public void Damage(float damage)
+    {
+        hp -= damage;
+        hpBar.fillAmount = hp / hpMax;
+        aud.PlayOneShot(soundHit);
+
+        if (hp <= 0)
+        {
+            final.SetActive(true);
+            textCount.text = "zombie you kill : " + count;
+        }
+    }
+    public void Replay()
+    {
+        SceneManager.LoadScene("遊戲場景");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Food/Assets/Scripts/Enemy.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+
+    [Header("速度"), Range(0, 10)]
+    public float speed = 1.5f;
+    [Header("停止距離"), Range(0, 5)]
+    public float stopDistance = 2;
+    [Header("追蹤目標名稱")]
+    public string targetName = "洋芋片";
+    [Header("爆炸效果")]
+    public GameObject explosion;
+    [Header("傷害"), Range(10, 100)]
+    public float damage;
+
+    private Transform target;
+    private NavMeshAgent nma;
+
+    private void Start()
+    {
+        nma = GetComponent<NavMeshAgent>();
+        nma.speed = speed;
+        nma.stoppingDistance = stopDistance;
+
+        target = GameObject.Find(targetName).transform;
+        nma.SetDestination(target.position);
+    }
+    private void CreateEffect()
+    { 
+        GameObject expel = Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(expel, 0.5f);
+        Destroy(gameObject);
+    }
+    private void Track()
+    {
+        nma.SetDestination(target.position);
+        if (nma.remainingDistance <= stopDistance)
+        {
+            CreateEffect();
+            target.GetComponent<ChipBag>().Damage(damage);
+        }
+    }
+    private void ClickAndDead()
+    {
+        CreateEffect();
+        ChipBag.target = transform;
+    }
+    private void OnMouseDown()
+    {
+        ClickAndDead();
+    }
+    private void Update()
+    {
+        Track();
+    }
+
+
+}
